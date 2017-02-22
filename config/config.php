42640e84289d19dd6c9156d4def909d638329573
Unknown <joshua@tabetozor.com>
2017-02-22 11:21:12 +0100

    add config

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+$db_name = "RPG";
+$user = "root";
+$pass = "root";
+
+try {
+    $db = new PDO('mysql:host='.$host.';dbname='.$db_name, $user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+}
+
+catch (EXCEPTION $e){
+    echo 'ERREUR:' .$e->getMessage().'<br>';
+    echo 'N°:'.$e->getCode();
+
+}
+?>
